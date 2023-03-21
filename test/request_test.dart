5eac504fe3f2db0208b562115f2b394896c15c49
mentally_unstable <islamdz2301@gmail.com>
2023-03-21 21:36:36 +0600

    feat: create dart_test.yaml
    - declare tests tags in dart_test.,yaml
    - add tags to the requests tests
    - add invalid requests group to test NetworkErrors

--- test/request_test.dart
+++ test/request_test.dart
@@ -11,6 +11,10 @@ import 'instance_test.dart';
 import 'models.dart';
 
 void main() {
+  const requestsTag = 'requests';
+  const validTag = 'valid';
+  const invalidTag = 'invalid';
+
   late NetworkOptions options;
   late NetworkService service;
   late Connection connection;
@@ -39,7 +43,7 @@ void main() {
       final Result resultList = await service.get('users', TestUser.fromJson);
 
       expect(resultList, isA<SuccessfulResult>());
-    });
+    }, tags: [requestsTag, validTag]);
 
     test('post request', () async {
       final post = TestPost(
@@ -62,7 +66,7 @@ void main() {
         (result as SuccessfulResult<TestPost>).data,
         post.copyWith(id: 101),
       );
-    });
+    }, tags: [requestsTag, validTag]);
 
     test('put request', () async {
       final post = TestPost(
@@ -83,12 +87,57 @@ void main() {
 
       expect(result, isA<SuccessfulResult>());
       expect((result as SuccessfulResult<TestPost>).data, post);
-    });
+    }, tags: [requestsTag, validTag]);
+
     test('delete request', () async {
       final result = await service.delete('posts/1');
 
       expect(result, isA<SuccessfulResult>());
-    });
+    }, tags: [requestsTag, validTag]);
+  });
+
+  group('Invalid requests', () {
+    setUp(() => {
+          connection = Connection(
+            connectivity: MockConnectivity(ConnectivityResult.wifi),
+          ),
+          options = NetworkOptions(
+            baseUrl: baseUrl,
+            connection: connection,
+          ),
+          service = NetworkService(options),
+        });
+
+    test('connection error', () async {
+      final mockOptions = options.copyWith(
+          connection: Connection(
+        connectivity: MockConnectivity(ConnectivityResult.none),
+      ));
+      service = NetworkService(mockOptions);
+
+      final Result result =
+          await service.get<TestPost>('posts/1', TestPost.fromJson);
+
+      expect(result, isA<ErrorResult>());
+      final error = (result as ErrorResult).error;
+      expect(error, isA<ConnectionNetworkError>());
+    }, tags: [requestsTag, invalidTag]);
+
+    test('type error', () async {
+      final Result result = await service.get('posts/1', TestUser.fromJson);
+
+      expect(result, isA<ErrorResult>());
+      final error = (result as ErrorResult).error;
+      expect(error, isA<TypeNetworkError>());
+    }, tags: [requestsTag, invalidTag]);
+
+    test('dio error', () async {
+      final Result result = await service.get('blank-url', TestPost.fromJson);
+
+      expect(result, isA<ErrorResult>());
+      final error = (result as ErrorResult).error;
+      expect(error, isA<DioNetworkError>());
+    }, tags: [requestsTag, invalidTag]);
   });
 }
 
